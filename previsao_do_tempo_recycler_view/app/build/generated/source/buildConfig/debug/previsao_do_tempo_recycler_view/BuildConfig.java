7264f682ff2d1ce69b189e103b1ffe75ed47aac9
ThielySantos <42822765+ThielySantos@users.noreply.github.com>
2019-12-01 13:49:46 -0300

    Add files via upload

---
+++ previsao_do_tempo_recycler_view/app/build/generated/source/buildConfig/debug/previsao_do_tempo_recycler_view/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package previsao_do_tempo_recycler_view;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.fiap_2tdsg_previsao_do_tempo_recycler_view";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
